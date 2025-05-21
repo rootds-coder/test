d5cea0f5a9027fe72d04f5d7cbba7b693fa26ac9
Dhruv <55385042+rootds-coder@users.noreply.github.com>
2025-05-21 14:44:05 +0100

    scam 404

--- app.js
+++ app.js
@@ -20,9 +20,12 @@ mongoose.connect(config.mongoUri, {
 // Middleware
 app.use(cors({
   origin: ['https://crowd-fund-zeta.vercel.app', 'http://localhost:3000'],
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
-  credentials: true
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'PATCH'],
+  allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With'],
+  exposedHeaders: ['Content-Range', 'X-Content-Range'],
+  credentials: true,
+  preflightContinue: false,
+  optionsSuccessStatus: 204
 }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
